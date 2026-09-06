f4a1f269a14e724e47865683503aead13031cad0
jeremie-78 <muchembled.jeremie78@gmail.com>
2026-09-06 14:08:44 +0200

    switched to node's native env loading

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,5 @@
 import "reflect-metadata";
 import { createServer, type Server } from "node:http";
-import dotenv from "dotenv";
-dotenv.config();
 import { type Express } from "express";
 import AppContainer from "./express";
 
